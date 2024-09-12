22b3acf5b5274f59c6ed779f0c67bfb5dcb3069a
OTI-OWOM <ezeogouba@gmail.com>
2024-09-11 22:15:09 -0700

    Added task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(99):
-    print("{:02}".format(i), end=",", " ")
+for i in range(0, 100):
+    if i != 99:
+        print("{:02}".format(i), end=",")
+
+    else:
+        print("{:02}".format(i))
