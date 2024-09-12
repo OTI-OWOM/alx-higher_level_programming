c210d155f72146df5df8357d4ee54380241935da
OTI-OWOM <ezeogouba@gmail.com>
2024-09-12 09:22:36 -0700

    Added Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
-def print_tebahpla():
-    result = ''
-    for i in range(26):
-        if i % 2 == 0:
-            result += '{}'.format(chr(122 - i))
-        else:
-            result += '{}'.format(chr(90 - i))
-    print(result, end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
