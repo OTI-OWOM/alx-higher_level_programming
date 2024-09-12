31f6868b76ef2c72102f6a167ea81de103ff1fcc
OTI-OWOM <ezeogouba@gmail.com>
2024-09-11 22:52:38 -0700

    Added task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,10 +2,9 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        if i != j
+        if i != j:
             if i == 8 and j == 9:
 
                 print("{:02}{}".format(i, j))
             else:
                 print("{:02}, {:02}".format(i, j), end=", ")
-
