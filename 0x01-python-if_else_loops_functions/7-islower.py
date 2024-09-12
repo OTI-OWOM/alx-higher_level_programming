3b00aba02d091fc4c13d078bec482fac3a4e6335
OTI-OWOM <ezeogouba@gmail.com>
2024-09-12 04:58:47 -0700

    Added Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def islower(c)
+def islower(c):
 
 
-for c in range (97, 123):
+for c in range(97, 122):
     if c == 99:
-        return("True")
+        return True
     else:
-        return("False")
+        return False
