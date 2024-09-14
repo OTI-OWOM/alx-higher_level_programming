4b7a879fc03f03d50f0ff8dc2efd761aa7ccfc19
OTI-OWOM <ezeogouba@gmail.com>
2024-09-14 11:23:03 -0700

    Added task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,11 +5,7 @@ if __name__ == "__main__":
     import hidden_4
     
     names = dir(hidden_4)
-
-    non_hidden_names = [name for name in names if not name.startswith("__")]
     
-    sorted_names = sorted(non_hidden_names)
-
-        for  name in sorted names:
+        for  name in names:
+            if name[:2] != "__":
             print(name)
-
