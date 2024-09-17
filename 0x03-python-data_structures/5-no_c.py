fc18b712e4d1c006e23074d2ddbe12d7a668f86e
OTI-OWOM <ezeogouba@gmail.com>
2024-09-17 16:31:40 -0700

    added task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 def no_c(my_string):
 
     new_string = ""
-        for char in my_string:
-            if chr != 'c' and chr != 'C':
-                new_string += char
-        return new_string
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
