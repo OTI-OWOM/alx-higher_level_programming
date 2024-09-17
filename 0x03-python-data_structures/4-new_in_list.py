6a3812d73ef71edea28d4091ceaa148bae6f625e
OTI-OWOM <ezeogouba@gmail.com>
2024-09-17 15:21:12 -0700

    Added task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,5 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:] 
     if 0 <= idx < len(my_list):
-        new_list[idx] = element:
-
-     print("{}".format.(new_list))
+        new_list[idx] = element
+            return new_list
