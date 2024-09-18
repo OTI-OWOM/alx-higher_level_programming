ce4efeecb8fd8d5f1c1d4543437ca923fe296ef7
OTI-OWOM <ezeogouba@gmail.com>
2024-09-18 03:39:24 -0700

    Added task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-def max_integer(my_list=[]):
-    for i in my_list:
-        if my_list = "":
-            return (None)
-        else:
-            return (max(i))
-
+def max_integer(my _list=[]):
+    if len(my_list) == 0:
+        return None
+    max_num = my_list[0]
+    for num in my_list[1:]
+        if num > max_num:
+            max_num = num
+    return max_num
