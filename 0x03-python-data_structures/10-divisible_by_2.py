a888e76c32942d9b0baee2cc729361a03e28e956
OTI-OWOM <ezeogouba@gmail.com>
2024-09-18 03:59:17 -0700

    Added task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    my_list = []:
     for i in my_list:
         if i % 2 == 0:
             return True
         else:
             return False
+    return new_list
