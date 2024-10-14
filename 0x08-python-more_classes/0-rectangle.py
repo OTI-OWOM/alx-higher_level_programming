cafb682f36eef71cecc64fd3bc9ea469722e277a
OTI-OWOM <ezeogouba@gmail.com>
2024-10-14 02:15:36 -0700

    Added task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+""" Write an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """creates class that defines a rectangle
+    Attributes:
+    empty
+    """
+    pass
