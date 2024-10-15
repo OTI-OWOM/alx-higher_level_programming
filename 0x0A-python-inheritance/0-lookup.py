f981a21a56fc241d3f9ef1d3b4f4b058e9c54562
OTI-OWOM <ezeogouba@gmail.com>
2024-10-15 08:35:35 -0700

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lookup Function"""
+
+
+def lookup(obj) -> list:
+    """Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to be scanned.
+
+    Returns:
+        lists: A list of the objects attributes and methods.
+    """
+
+    return dir(obj)
