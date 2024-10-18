985d182e9c5b6ae0b3debd86c78dc8c92b47cdbd
OTI-OWOM <ezeogouba@gmail.com>
2024-10-18 16:24:16 -0700

    Added task 8

---
+++ 0x0B-python-input_output/8-my_class_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" My class module
+"""
+
+
+class MyClass:
+    """ My class
+    """
+
+    score = 0
+
+    def __init__(self, name, number=4):
+        self.__name = name
+        self.number = number
+        self.is_team_red = (self.number % 2) == 0
