607114aa4893311330509a6d6a95214f1d2fdc11
OTI-OWOM <ezeogouba@gmail.com>
2024-10-19 01:33:00 -0700

    Added task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Student Module"""
+
+
+class Student:
+    """Defines a Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of a Student instance.
+
+        Args:
+            attrs (list[str], optional): Attrs to be shown. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        if isinstance(attrs, list) and \
+                (all(isinstance(s, str) for s in attrs)):
+             return {k: v for k, v in dict(self.__dict__).items() if k in attrs}
+
+        return self.__dict__
