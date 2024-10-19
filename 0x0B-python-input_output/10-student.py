4672b00ea8b7f7d4d422f10224463f46e9731dce
OTI-OWOM <ezeogouba@gmail.com>
2024-10-19 00:37:43 -0700

    Added task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,18 +3,14 @@
 
 
 class Student:
-    """Defines a student class."""
+    """Defines a Student class."""
 
-    def  __init__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-
-        def to_json(self, attrs=None):
-            """Return the dictionary representation of a student instance."""   
-            if isinstance(attrs, list) and \
-                    (all(isinstance(s, str) for s in attrs)):
-                return {k: v for k, v in dict(self._dict_).items() if k in attrs}
-
-            return self.__dict__
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of a Student instance."""
+        if isinstance(attrs, list) and \
+                (all(isinstance(s, str) for s in attrs)):
