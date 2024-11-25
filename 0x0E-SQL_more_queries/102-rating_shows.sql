384787aa3092db5084499eac96be9bd37b92e110
OTI-OWOM <ezeogouba@gmail.com>
2024-11-25 03:00:37 -0800

    feat: 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,7 @@
--- List all shows sorted by their total rating
-SELECT tv_shows.title, SUM(tv_show_rates.rate) AS rating
+-- Script that lists shows by their rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating -- Query to join tables
 FROM tv_shows
-JOIN tv_show_rates ON tv_shows.id = tv_show_rates.show_id
-GROUP BY tv_shows.id, tv_shows.title
+JOIN tv_show_ratings
+     ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
 ORDER BY rating DESC;
