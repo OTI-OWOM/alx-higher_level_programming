09101ff9411718f3cce7b4a070a5816b3fdac5e6
OTI-OWOM <ezeogouba@gmail.com>
2024-11-25 01:09:01 -0800

    add: 5-unique_id

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 -- Create the table unique_id if it doesn't already exist
 -- Ensure the table has an id column with a default value of 1 and is unique
 -- and a name column with no constraints
-CREATE TABLE IF NOT EXISTS unique_1d (
+CREATE TABLE IF NOT EXISTS unique_id (
 	id INT DEFAULT 1 UNIQUE,
 	name VARCHAR(256)
 );
