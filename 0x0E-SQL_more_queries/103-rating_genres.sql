3f0adab3f26eb6b046f828bc798fd98fabc1c661
OTI-OWOM <ezeogouba@gmail.com>
2024-11-25 03:13:51 -0800

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,11 @@
 -- List all genres sorted by their total rating
 SELECT tv_genres.name, SUM(tv_show_rates.rate) AS rating
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_show_rates ON tv_shows.id = tv_show_rates.show_id
+JOIN tv_show_genres 
+     ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows 
+     ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_show_rates
+     ON tv_shows.id = tv_show_rates.show_id
 GROUP BY tv_genres.id, tv_genres.name
 ORDER BY rating DESC;
