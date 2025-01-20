1cd3701c52f10fc0eff4bce8e617e6675cd849ef
OTI-OWOM <ezeogouba@gmail.com>
2025-01-20 05:34:58 -0800

    feat:4. cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to send a GET request with a custom header and display the response body
-curl -s -H "X-School-User -Id: 98" "$1"
+# This script takes in a URL and sends a GET request to the URL, and displays the body of the response
+curl -s -H "X-School-User-Id: 98" -X GET "$1"
