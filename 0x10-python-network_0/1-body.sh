c00c03940c86dba67d45d8f295b59be4000adc1e
OTI-OWOM <ezeogouba@gmail.com>
2025-01-20 03:24:12 -0800

    feat: 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of a 200 status code response
-curl -s -w "%{http_code}" "$1" | grep -E "^200$" > /dev/null && curl -s "$1"
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL -X GET "$1"
