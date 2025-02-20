7295cce422964f04e2ed7ff4ebfd83d255c98943
OTI-OWOM <ezeogouba@gmail.com>
2025-02-20 06:51:12 -0800

    feat: readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,17 +8,17 @@ const filePath = process.argv[2];
 
 // Check if the file path is provided
 if (!filePath) {
-console.error('usage: node 0-readme.js <file_path>');
-process.exit(1);
+  console.error('usage: node 0-readme.js <file_path>');
+  process.exit(1);
 }
 
 // Read thr file in UTF-8 encoding
 fs.readFile(filePath, 'utf-8', (err, data) => {
-if (err) {
+  if (err) {
 // Print the error object if an error occurs
-console.error(err);
-} else {
+    console.error(err);
+  } else {
 // print the file content
-console.log(data);
-}
+    console.log(data);
+  }
 });
