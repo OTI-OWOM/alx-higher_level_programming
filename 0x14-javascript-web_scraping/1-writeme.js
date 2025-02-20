1b48bbb4dc26eba777e05121c70d22cbafe5d432
OTI-OWOM <ezeogouba@gmail.com>
2025-02-20 07:20:47 -0800

    feat: writeme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const fs = require('process');
+
+// Get the file path and string from the arguments
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Check if both file path and content are provided
+if (!filePath || !content) {
+  console.error('Usage: node 1-writeme.js <file-path> "<string_to_write>"');
+  process.exit(1);
+}
+
+// Write the string to the file in UTF-8 encoding
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    // Print the error object if an error occurs
+    console.error(err);
+  } else {
+	  // Print a sucess message
+	 console.log('string written to ${filePath}');
+  }
+});
