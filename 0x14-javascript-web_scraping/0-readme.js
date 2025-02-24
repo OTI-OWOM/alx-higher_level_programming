374d05cf7ce39fe45aa0d0fd40a182a51753ebb4
OTI-OWOM <ezeogouba@gmail.com>
2025-02-24 19:02:38 +0100

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const process = require('process');
 
-// Get the file path from the first argument
-const filePath = process.argv[2];
-
-// Check if the file path is provided
-if (!filePath) {
-	console.error('usage: node 0-readme.js <file_path>');
-	process.exit(1);
+// Check if the file path argument is provided
+if (process.argv.length < 3) {
+	  console.error('Usage: node read-file.js <file-path>');
+	  process.exit(1);
 }
 
-// Read thr file in UTF-8 encoding
+// Get the file path from command line arguments
+const filePath = process.argv[2];
+
+// Read the file in UTF-8 encoding
 fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		// Print the error object if an error occurs
-		console.error(err);
-	} else {
-		// print the file content
+	 if (err) {
+		 console.error(err);
+	 } else {
 		console.log(data);
 	}
 });
